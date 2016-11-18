b924001f6e690f688bcaf04af78022d72a3895b9
Jeffrey Maggio <jmaggio14@gmail.com>
2016-11-18 03:15:56 -0500

    adds jean's equations *UNTESTED*

---
+++ src/Jeans.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sam
+
+def jeans_mass(u,T,rho):
+	"""
+	calculates the jean's mass of an ISM cloud
+	Mj = ( (5kT) / (G*µ*mH) )^(3/2) * (3 / (4*pi*rho) )
+
+	where k is Boltzmann, T is temperature, G is Newton's constant
+	µ is the number of nucleons (?), mH is mass of hydrogen
+	rho is the density of the cloud
+
+	WIP -- needs auto-error checking
+	"""
+	firstTerm = ( (5 * sam.SAM_k * T) / (sam.SAM_G * u * sam.SAM_mh) )**1.5
+	secondTerm =  (3 / (4 * sam.SAM_pi * rho) )**.5 
+	Mj = firstTerm * secondTerm
+	return Mj
+
+def jeans_mass2(u,T,n):
+	"""
+	calculates the jean's mass of an ISM cloud, uses 
+	Mj = ( (375 k^3) / (4 * pi * (µ*mH)^4 * G^3) )
+
+	WIP -- needs auto-error checking
+	"""
